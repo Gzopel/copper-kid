dd059eca4ed32dac3dab29fe891bad8ed198b16f
gzopel <jepunto@gmail.com>
2016-10-28 10:56:23 -0300

    Parsing building countdown.

--- tests/resourcesPageParser.test.js
+++ tests/resourcesPageParser.test.js
@@ -5,27 +5,52 @@ import resourcesPageParser from '../lib/parsers/resourcesPageParser';
 import { CODES } from '../lib/GameElements';
 
 const resourcesPage =  fs.readFileSync(path.resolve('testPages','resources.html'), { encoding: 'utf8' });
+const resourcesPage2 =  fs.readFileSync(path.resolve('testPages','resources2.html'), { encoding: 'utf8' });
 
 describe(__filename, () => {
   it('Should parse the test page', (done) => {
     const parseResult = resourcesPageParser(resourcesPage);
-    assert.equal(parseResult.planetName, 'Homeworld');
-    assert.equal(parseResult.planetId, '33628551');
-    assert.equal(parseResult.coordinates.galaxy, 2);
-    assert.equal(parseResult.coordinates.system, 31);
-    assert.equal(parseResult.coordinates.planet, 12);
-    assert.equal(parseResult.resources.metal, 65);
-    assert.equal(parseResult.resources.crystal, 261);
-    assert.equal(parseResult.resources.deuterium, 0);
-    assert.equal(parseResult.resources.energy, 47);
-    assert.equal(parseResult.buildings[CODES.METAL_MINE], 4);
-    assert.equal(parseResult.buildings[CODES.CRYSTAL_MINE], 1);
-    assert.equal(parseResult.buildings[CODES.DEUTERIUM_SYNTHETIZER], 0);
-    assert.equal(parseResult.buildings[CODES.SOLAR_PLANT], 4);
-    assert.equal(parseResult.buildings[CODES.METAL_STORAGE], 0);
-    assert.equal(parseResult.buildings[CODES.CRYSTAL_STORAGE], 0);
-    assert.equal(parseResult.buildings[CODES.DEUTERIUM_TANK], 0);
-    assert.equal(parseResult.ships[CODES.SOLAR_SATELLITE], 0);
+    assert.equal(parseResult.planet.planetName, 'Homeworld');
+    assert.equal(parseResult.planet.planetId, '33628551');
+    assert.equal(parseResult.planet.buildingCountdown, 0);
+    assert.equal(parseResult.planet.coordinates.galaxy, 2);
+    assert.equal(parseResult.planet.coordinates.system, 31);
+    assert.equal(parseResult.planet.coordinates.planet, 12);
+    assert.equal(parseResult.planet.resources.metal, 65);
+    assert.equal(parseResult.planet.resources.crystal, 261);
+    assert.equal(parseResult.planet.resources.deuterium, 0);
+    assert.equal(parseResult.planet.resources.energy, 47);
+    assert.equal(parseResult.planet.buildings[CODES.METAL_MINE], 4);
+    assert.equal(parseResult.planet.buildings[CODES.CRYSTAL_MINE], 1);
+    assert.equal(parseResult.planet.buildings[CODES.DEUTERIUM_SYNTHETIZER], 0);
+    assert.equal(parseResult.planet.buildings[CODES.SOLAR_PLANT], 4);
+    assert.equal(parseResult.planet.buildings[CODES.METAL_STORAGE], 0);
+    assert.equal(parseResult.planet.buildings[CODES.CRYSTAL_STORAGE], 0);
+    assert.equal(parseResult.planet.buildings[CODES.DEUTERIUM_TANK], 0);
+    assert.equal(parseResult.planet.ships[CODES.SOLAR_SATELLITE], 0);
     done();
-  })
+  });
+
+  it('Should parse the building construction countdown', (done) => {
+    const parseResult = resourcesPageParser(resourcesPage2);
+    assert.equal(parseResult.planet.planetName, 'Homeworld');
+    assert.equal(parseResult.planet.planetId, '33628551');
+    assert.equal(parseResult.planet.buildingCountdown, 2 * 60 + 28);
+    assert.equal(parseResult.planet.coordinates.galaxy, 2);
+    assert.equal(parseResult.planet.coordinates.system, 31);
+    assert.equal(parseResult.planet.coordinates.planet, 12);
+    assert.equal(parseResult.planet.resources.metal, 7065);
+    assert.equal(parseResult.planet.resources.crystal, 6872);
+    assert.equal(parseResult.planet.resources.deuterium, 1778);
+    assert.equal(parseResult.planet.resources.energy, 9);
+    assert.equal(parseResult.planet.buildings[CODES.METAL_MINE], 8);
+    assert.equal(parseResult.planet.buildings[CODES.CRYSTAL_MINE], 5);
+    assert.equal(parseResult.planet.buildings[CODES.DEUTERIUM_SYNTHETIZER], 3);
+    assert.equal(parseResult.planet.buildings[CODES.SOLAR_PLANT], 8);
+    assert.equal(parseResult.planet.buildings[CODES.METAL_STORAGE], 0);
+    assert.equal(parseResult.planet.buildings[CODES.CRYSTAL_STORAGE], 0);
+    assert.equal(parseResult.planet.buildings[CODES.DEUTERIUM_TANK], 0);
+    assert.equal(parseResult.planet.ships[CODES.SOLAR_SATELLITE], 0);
+    done();
+  });
 });
