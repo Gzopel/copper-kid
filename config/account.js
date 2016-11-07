59f9bab26cc0b6626c204c73decc11277f2e79fa
gzopel <jepunto@gmail.com>
2016-11-07 18:43:39 -0300

    General refactor

--- config/account.js
+++ config/account.js
@@ -3,6 +3,9 @@ export const account = {
   password: process.env.PASSWORD,
   universeName: process.env.UNIVERSE,
   server: 'en.ogame.gameforge.com',
+  spyProbes: 3,
+  attackRange: 25,
+  reservedFleetSlots: 0,
 };
 
 export default account;
