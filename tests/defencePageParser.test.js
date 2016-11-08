f2b2dbabe9a7418aed26b163b64883b117d8a1a7
gzopel <jepunto@gmail.com>
2016-11-08 14:52:18 -0300

    Game element refactor

--- tests/defencePageParser.test.js
+++ tests/defencePageParser.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { assert } from 'chai';
-import { GAME_ELEMENTS } from '../lib/GameElements';
+import { CODES } from '../lib/GameElements';
 import { defencePageParser } from '../lib/GameClient/parsers/defencePageParser';
 
 const defencePage =  fs.readFileSync(path.resolve('testPages', 'defense.html'), { encoding: 'utf8' });
@@ -21,15 +21,15 @@ describe(__filename, () => {
     assert.equal(parseResult.planet.resources.crystal, 6193);
     assert.equal(parseResult.planet.resources.deuterium, 9580);
     assert.equal(parseResult.planet.resources.energy, 160);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.ROCKET_LAUNCHER.code], 60);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.LIGHT_LASER.code], 40);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.HEAVY_LASER.code], 5);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.GAUSS_CANNON.code], 0);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.ION_CANNON.code], 0);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.SMALL_SHIELD_DOME.code], 1);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.LARGE_SHIELD_DOME.code], 0);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.ANTI_BALLISTIC_MISSILE.code], 0);
-    assert.equal(parseResult.planet.defences[GAME_ELEMENTS.INTERPLANETARY_MISSILE.code], 0);
+    assert.equal(parseResult.planet.defences[CODES.ROCKET_LAUNCHER], 60);
+    assert.equal(parseResult.planet.defences[CODES.LIGHT_LASER], 40);
+    assert.equal(parseResult.planet.defences[CODES.HEAVY_LASER], 5);
+    assert.equal(parseResult.planet.defences[CODES.GAUSS_CANNON], 0);
+    assert.equal(parseResult.planet.defences[CODES.ION_CANNON], 0);
+    assert.equal(parseResult.planet.defences[CODES.SMALL_SHIELD_DOME], 1);
+    assert.equal(parseResult.planet.defences[CODES.LARGE_SHIELD_DOME], 0);
+    assert.equal(parseResult.planet.defences[CODES.ANTI_BALLISTIC_MISSILE], 0);
+    assert.equal(parseResult.planet.defences[CODES.INTERPLANETARY_MISSILE], 0);
     assert.equal(parseResult.enemyAttacks.length, 0);
     done();
   });
